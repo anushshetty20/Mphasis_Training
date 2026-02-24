4c7ee89d2fe769b1552cbad501a13878c377ec49
Anush X <anush.x@mphasis.com>
2026-02-24 16:48:35 +0530

    Bean life cycle methods

---
+++ BeanLifeCycleMethods/src/main/java/com/test/ConnectionManager.java
@@ -0,0 +1,34 @@
+package com.test;
+
+import org.springframework.stereotype.Component;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
+@Component
+public class ConnectionManager {
+	private boolean isConnected = false;
+	
+	@PostConstruct
+	public void connect() {
+		System.out.println("Connecting to external service");
+		isConnected = true;
+		System.out.println("Connection established isConnected : "+isConnected);
+	}
+	
+	@PreDestroy
+	public void disconnect() {
+		System.out.println("Disconnecting from external service");
+		isConnected = false;
+		System.out.println("Connection closed isConnected : "+isConnected);
+	}
+	
+	@PreDestroy
+	public void operation() {
+		if(isConnected) {
+			System.out.println("Performing operation with active external server");
+		}else {
+			System.out.println("operation failed not connected");
+		}
+	}
+}
